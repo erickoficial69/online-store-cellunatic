214192f9a51768a55da4049d0229517fee133afb
erickoficial69 <erickoficial69@gmail.com>
2021-01-06 01:12:24 -0400

    API set

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,7 +2,7 @@ import App,{AppProps,AppContext} from "next/app";
 import {useEffect,useState} from 'react'
 import { User, AppData, TasaCambio } from '../interfaces/interfaces'
 
-const API:any = process.env.CELLUNATICBACKEND || 'http://localhost:3001' 
+const API:any = process.env.API || 'http://localhost:3001' 
 
 const initialApp={
   name:'',
